fa3f451f2fb9c9e61a58ee5d31c7611b94622e62
urufi <86318095+urufi@users.noreply.github.com>
2021-09-13 01:03:45 +0200

    Add files via upload

---
+++ RADIUS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Somme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            double  r,per_cir;
+            Console.WriteLine("enter the radius of a circle: ");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            per_cir = 2 * Math.PI * r;
+            Console.WriteLine("perimeter of a circle is: {0}", per_cir);
+            Console.Read();
+
+
+
+
+
+        }
+
+        
+    }
+}
