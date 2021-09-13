329b41d04534c35079c5d4f424bf91e9ed0f90da
urufi <86318095+urufi@users.noreply.github.com>
2021-09-13 02:04:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace radius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Enter r the radius of the cricle");
+            int r = int.Parse(Console.ReadLine());
+
+            double per_cir = (double) (2 * Math.PI * r);
+            double area = (double) (Math.PI*r*r);
+            Console.WriteLine("The perimeter and area of the cirlce are " + per_cir +" and " + area);
+
+
+
+        }
+    }
+}
