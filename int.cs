395fd3c69b0afdf19cc3c70a398d9102e990d528
urufi <86318095+urufi@users.noreply.github.com>
2021-09-14 05:43:10 +0200

    Update int.cs

--- int.cs
+++ int.cs
@@ -1,4 +1,7 @@
-﻿
+//Write a program that prints three numbers in three virtual columns on the consoole. Each column should have a width of 10 characters.
+//and the numbers should be left aligned. The first number should be an integer in hexadecimal; the second should be fractional positive; and the third a negative fraction.
+//The two numbers have to be rounded to the second decimal place.
+
 using System;
 
 namespace exos
@@ -7,11 +10,11 @@ namespace exos
     {
         static void Main(string[] args)
         {
-            int hexnum = 2013;
-            Console.WriteLine("|{0,-10}|", hexnum);
-            double fracpo = 0.256;
+            int hexnum = 15;
+            Console.WriteLine("|{0,-10:x}|", hexnum);
+            double fracpo = 0.25;
             Console.WriteLine("|{0,-10:f2}|", fracpo);
-            double fracne = -0.589;
+            double fracne = -0.59;
             Console.WriteLine("|{0,-10}|", fracne);
         }
     }
