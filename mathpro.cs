359060f3b5489685a35e4f4f49f585a02de469f4
urufi <86318095+urufi@users.noreply.github.com>
2021-09-14 05:44:18 +0200

    Add files via upload

---
+++ mathpro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+//Write a program that reads from the console two integer numbers (int) and prints how many numbers between them exis, such that the remainder of their division by 5 is 0.
+
+namespace math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("enter n :");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter m :");
+            int m =  int.Parse(Console.ReadLine());
+            int x = 0;
+           
+
+            while (n<=m) 
+            {
+                if (n % 5 == 0)
+     
+                {
+                    x = x + 1; 
+                }
+                n = n + 1;
+             
+            }
+
+            Console.WriteLine(x);
+        }
+    }
+}
+
