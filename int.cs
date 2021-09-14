bd17ff3c662de0036c6126d9d465d6c25bce1f3a
urufi <86318095+urufi@users.noreply.github.com>
2021-09-14 03:43:48 +0200

    Add files via upload

---
+++ int.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace exos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hexnum = 2013;
+            Console.WriteLine("|{0,-10}|", hexnum);
+            double fracpo = 0.256;
+            Console.WriteLine("|{0,-10:f2}|", fracpo);
+            double fracne = -0.589;
+            Console.WriteLine("|{0,-10}|", fracne);
+        }
+    }
+}
