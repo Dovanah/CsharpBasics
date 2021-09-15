8731ad2f43c673874509f216d8383ca4e4168b00
urufi <86318095+urufi@users.noreply.github.com>
2021-09-15 02:48:09 +0200

    Add files via upload

---
+++ phy7.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace physicsu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            Console.WriteLine(" enter an integer number 1: ");
+            long x = long .Parse(Console.ReadLine());
+            Console.WriteLine(" enter an integer number 2: ");
+            long y = long.Parse(Console.ReadLine());
+            Console.WriteLine(" enter an integer number 3: ");
+            long z = long.Parse(Console.ReadLine());
+            Console.WriteLine(" enter an integer number 4: ");
+            int n = long.Parse(Console.ReadLine());
+            Console.WriteLine(" enter an integer number 5: ");
+            long m = long.Parse(Console.ReadLine());
+
+           Console.WriteLine("{0}+{1}+{2}+{3}+{4}={5}", x, y, z, n, m, x + y + z + n + m);
+
+        }
+    }
+}
