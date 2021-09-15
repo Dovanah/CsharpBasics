ede06226b9d40b29293be3b1ad013de7d86a7d69
urufi <86318095+urufi@users.noreply.github.com>
2021-09-15 02:49:55 +0200

    Update phy7.cs

--- phy7.cs
+++ phy7.cs
@@ -8,7 +8,9 @@ namespace physicsu
         {
 
 
-
+    //7. Write a program that reads five integer numbers and prints their
+    //sum. If an invalid number is entered the program should prompt the user
+    //to enter another number.
 
             Console.WriteLine(" enter an integer number 1: ");
             long x = long .Parse(Console.ReadLine());
